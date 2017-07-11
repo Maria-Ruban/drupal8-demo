17b17a6e321e7d40555f6c7779addac4017e72c1
Prashanth <prashanth@infanion.com>
2017-07-11 17:25:37 +0530

    Added features module

--- db_migration.sh
+++ db_migration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd src
 
-#drush --yes en demo config_update features database_configuration
+drush --yes en demo config_update features database_configuration
 
 # Clear cache after db changes
 drush cache-rebuild
